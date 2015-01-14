e536b11fa2558d01731ee0991f0ab0e5a1881b97
Kiffin Gish <kiffin.gish@planet.nl>
2015-01-14 15:37:14 +0100

    All the top level templates are working

--- app/router.js
+++ app/router.js
@@ -1,11 +1,22 @@
 import Ember from 'ember';
-import config from 'config/environment';
+import config from './config/environment';
 
 var Router = Ember.Router.extend({
   location: config.locationType
 });
 
 Router.map(function() {
+  this.route("help");
+  this.route("about");
+  this.resource("users", function() {
+    this.route("signup");
+    this.route("user");
+  });
+  this.resource("sessions", function() {
+    this.route("logout");
+    this.route("login");
+  });
+  this.route("secret");
 });
 
 export default Router;
