ac227bc284e02f9b8d22980d4940632e94b7cbc7
Kiffin Gish <kiffin.gish@planet.nl>
2015-01-16 15:23:15 +0100

    All helpers have been migrated.

--- app/helpers/formatvalue.js
+++ app/helpers/formatvalue.js
@@ -1,13 +1,13 @@
 import Ember from 'ember';
 
-export function formatvalue(input) {
+export function formatvalue(input, options) {
   var placeholder = options.hash['placeholder'];
   if (typeof input === 'undefined') {
-    return placeholder || '[UNDEFINED]'
+    return placeholder || '[UNDEFINED]';
   } else if (input === null) {
-    return placeholder || '[NULL]'
+    return placeholder || '[NULL]';
   } else if (typeof input === 'string' && input.length === 0) {
-    return placeholder || '[EMPTY]'
+    return placeholder || '[EMPTY]';
   } else {
     return input;
   }
