c21c1d5c0f6cb73b71a18cc19e801d86b92ccecb
Kiffin Gish <kiffin.gish@planet.nl>
2015-01-16 14:31:01 +0100

    User and product templates and routes migrated.

--- app/router.js
+++ app/router.js
@@ -8,15 +8,24 @@ var Router = Ember.Router.extend({
 Router.map(function() {
   this.route("help");
   this.route("about");
-  this.resource("users", function() {
-    this.route("signup");
-    this.route("user");
+
+  this.resource('users', function() {
+    this.route('signup');
+    this.route('user', { path: '/profile/:user_id' });
   });
+
   this.resource("sessions", function() {
     this.route("logout");
     this.route("login");
   });
   this.route("secret");
+
+  this.resource('products', function() {
+    this.route('new');
+    this.resource('product', { path: ':product_id' }, function() {
+      this.route('edit');
+    });
+  });
 });
 
 export default Router;
