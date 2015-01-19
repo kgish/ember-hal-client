97b1d517ecf7940f51ae6aae689442f8f33b9891
Kiffin Gish <kiffin.gish@planet.nl>
2015-01-19 14:32:11 +0100

    Completed upgrade to ember-cli 0.1.7, still some minor stuff to do but most important features have been implemented.

--- app/routes/secret.js
+++ app/routes/secret.js
@@ -1,10 +1,6 @@
-// import Ember from 'ember';
-
-// TODO: Only allow admin to transition to this route.
 import Authenticated from './authenticated';
 
 export default Authenticated.extend({
-//export default Ember.Route.extend({
     model: function() {
         // Instantiate the model for the SecretController as a list of created users
         console.log('SecretRoute: model()');
