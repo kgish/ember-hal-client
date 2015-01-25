36c24d8e9cdaea9eeb6a3793dabadc5244438ec8
Kiffin Gish <kiffin.gish@planet.nl>
2015-01-25 17:30:55 +0100

    Added a new root page linked to the hostname on navigation bar which displays the response from the hal server.

--- app/routes/root.js
+++ app/routes/root.js
@@ -1,4 +1,23 @@
 import Ember from 'ember';
+import config from './../config/environment';
 
 export default Ember.Route.extend({
+    model: function() {
+        var url = config.APP.RESTADAPTER_HOST;
+        console.log('RootRoute: model() => getJSON('+url+')');
+        Ember.$.ajaxSetup({ contentType: 'application/json; charset=utf-8' });
+        var _this = this;
+        return Ember.$.getJSON(url).then(
+            function(response) {
+                console.log('RootRoute: model() => OK, response='+JSON.stringify(response));
+                return JSON.stringify(response, null, '  ');
+            },
+            function(error) {
+                // TODO Handle errors better in gui
+                var errmsg = 'RootRoute: model() => NOK, error='+JSON.stringify(error);
+                console.log(errmsg);
+                _this.transitionTo('error');
+            }
+        );
+    }
 });
