e180b00e011536a76ccaa7ba467ae01a9330edb6
Kiffin Gish <kiffin.gish@planet.nl>
2015-02-06 21:18:31 +0100

    Deleted leftover files which are no longer needed.

--- app/routes/_secret.js
+++
@@ -1,9 +0,0 @@
-import Authenticated from './authenticated';
-
-export default Authenticated.extend({
-    model: function() {
-        // Instantiate the model for the SecretController as a list of created users
-        console.log('SecretRoute: model()');
-        return this.store.find('user');
-    }
-});
--- app/routes/users/_index.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Route.extend({
-    beforeModel: function() {
-        console.log('UsersIndexRoute: beforeModel() => transitionTo(\'users\')');
-        this.transitionTo('users');
-    }
-});
